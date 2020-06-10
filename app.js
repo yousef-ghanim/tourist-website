dfe48f3c8c0444667d0edc42fcc2f7aaf7b17d35
yousef-ghanim <yousefghanim85@gmail.com>
2020-06-10 13:12:57 +0000

    nav ready try 1

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ Array.from(document.querySelectorAll(".tours-btn")).forEach((item) => {
     item.parentElement.parentElement.classList.toggle("change");
   };
 });
+
+const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn");
+const container = document.querySelector(".web-container");
+openBtn.addEventListener("click", () => {
+  container.classList.add("change");
+});
+closeBtn.addEventListener("click", () => {
+  container.classList.remove("change");
+});
